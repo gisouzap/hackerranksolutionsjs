40acbd2aea0ceac550897dd072a3d1054c9f7f64
Giselle Souza <gisellesouz.ti@gmail.com>
2019-11-01 10:17:28 -0300

    pi

---
+++ 10DaysofJavascript/Day 1: Let and Const/pi.js
@@ -0,0 +1,11 @@
+ // Write your code here. Read input using 'readLine()' and print output using 'console.log()'.
+    const PI = Math.PI;
+    var r = readLine();
+    // Print the area of the circle:
+    var area;
+    area = PI * (r ** 2);
+    console.log(area);
+    // Print the perimeter of the circle:
+    var perimeter;
+    perimeter = 2 * PI * r;
+    console.log(perimeter);
